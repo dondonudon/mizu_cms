0e640ff0a1e1e846ca93c12f8d03982033895aba
Prasetyo <alvianto.prasetyo@live.com>
2020-11-05 16:28:33 +0700

    add firebase config

---
+++ src/FIREBASE_CONFIG.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDUiHJ1JUKc_MdBpJgqfG2PFJdxw7kZqCA",
+    authDomain: "mizu-ee813.firebaseapp.com",
+    databaseURL: "https://mizu-ee813.firebaseio.com",
+    projectId: "mizu-ee813",
+    storageBucket: "mizu-ee813.appspot.com",
+    messagingSenderId: "244281596078",
+    appId: "1:244281596078:web:9a62823848b79ebc99353f",
+    measurementId: "G-0RHYF75GFL"
+};
